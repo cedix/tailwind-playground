2c448a7fd0a60999e376b2785e4a8cb227d9b9a3
Cedric Texier <cedix@icloud.com>
2019-11-12 09:11:09 +0100

    Initial commit

---
+++ components/VCollapsible.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :class="['collapsible', 'rounded', isExpanded ? 'expanded' : '']">
+    <div class="title bg-gray-700 text-gray-200 cursor-pointer" @click="toggleCollapsible">
+      <div class="px-4 py-2">Title</div>
+    </div>
+    <div class="content bg-gray-600 text-gray-200">
+      <div class="px-4 py-2">Content</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    expanded: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      isExpanded: this.expanded
+    };
+  },
+
+  methods: {
+    toggleCollapsible() {
+      this.isExpanded = !this.isExpanded;
+    }
+  }
+};
+</script>
+
+<style>
+:root {
+  --animation-duration: 0.5s;
+}
+</style>
+
+
+<style scoped>
+.collapsible {
+  overflow: hidden;
+}
+.collapsible > .content {
+  max-height: 0;
+  transition: max-height var(--animation-duration) ease-in-out;
+  overflow: hidden;
+}
+.collapsible.expanded > .content {
+  max-height: 40px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1 class="flex justify-center text-xl font-bold">NUXTJS / Tailwind CSS Template</h1>
+    <div class="px-8">
+      <collapsible class="mt-8"/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Collapsible from "@/components/VCollapsible.vue";
+export default {
+  components: {
+    Collapsible
+  }
+};
+</script>
+
+<style scoped>
+</style>
